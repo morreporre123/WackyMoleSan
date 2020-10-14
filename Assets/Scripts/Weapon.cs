37cd73d65a8e8176d12ca49a9d0e331062baeb62
morreporre123 <mauritz.backman@learnet.se>
2020-10-14 10:55:05 +0200

    Musgrejer(som inte fungerar)

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,35 +9,69 @@ public class Weapon : MonoBehaviour
     //Nummer för vilket vapen man väljer, 1 är fist, 2 är chainsaw och 3 är gun //Mauritz
     public int weaponnumber;
 
-    // Start is called before the first frame update
-    void Start()
+    public Sprite fist;
+    public Sprite chainsaw;
+    public Sprite gun;
+    public SpriteRenderer cs; //CursorSprite
+
+    private Vector3 cursorposition;
+    private float mouseX;
+    private float mouseY;
+
+    public Camera cam;
+
+
+
+    private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        mouseX = Input.mousePosition.x;
+        mouseY = Input.mousePosition.y;
+        cursorposition = Input.mousePosition;
+        cursorposition.z = 0;
+        cursorposition = cam.ScreenToWorldPoint(cursorposition);
 
-    void Fist()
+        cs.transform.position = cursorposition;
+
+        if (weaponnumber == 1)
+        {
+            cs.sprite = fist;
+
+        }
+        if(weaponnumber == 2)
+        {
+            cs.sprite = chainsaw;
+
+        }
+        if(weaponnumber == 3)
+        {
+            cs.sprite = gun;
+
+        }
+    }
+    #region Funkytions
+    public void Fist()
     {
         weaponnumber = 1;
-        SceneManager.LoadScene(sceneBuildIndex: +1);
+        SceneManager.LoadScene("Testscen123");
     }
 
-    void Chainsaw()
+    public void Chainsaw()
     {
         weaponnumber = 2;
-        SceneManager.LoadScene(sceneBuildIndex: +1);
+        SceneManager.LoadScene("Testscen123");
     }
 
-    void Gun()
+    public void Gun()
     {
         weaponnumber = 3;
-        SceneManager.LoadScene(sceneBuildIndex: +1);
+        SceneManager.LoadScene("Testscen123");
     }
-
+    #endregion
 
 }
