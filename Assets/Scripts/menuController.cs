8dffc77f3241f60cd58394f8133f512917e889ec
LucasBang04 <56590546+LucasBang04@users.noreply.github.com>
2020-10-20 14:58:44 +0200

    menu commit

---
+++ Assets/Scripts/menuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour
+{
+    public string playableSceneName;
+    public void play()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+    public void exit()
+    {
+        Application.Quit();
+        //För att stänga spelet i editor:
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
